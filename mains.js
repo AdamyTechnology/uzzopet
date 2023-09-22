c7f0b10d8628573abbf76b27799499da14bbd5e4
Rafa Adamy <95262339+AdamyTechnology@users.noreply.github.com>
2023-09-22 19:51:26 -0300

    Add files via upload

---
+++ mains.js
@@ -0,0 +1,61 @@
+const sr = ScrollReveal({ reset: true })
+
+sr.reveal('.avatar', {
+  origin: 'top',
+  distance: '100px', // A distancia do origin para a posição original do elemento.
+  duration: 1200, // A duração da animação em ms
+  delay: 100, // O delay para a animação acontecer
+  reset: false // Ao sair do campo de tela, a animação não acontecerá novamente caso eu passe por ela novamente.
+})
+
+sr.reveal('.nome', {
+  origin: 'left',
+  distance: '500px', // A distancia do origin para a posição original do elemento.
+  duration: 1200, // A duração da animação em ms
+  delay: 300, // O delay para a animação acontecer
+  reset: false // Ao sair do campo de tela, a animação não acontecerá novamente caso eu passe por ela novamente.
+})
+
+sr.reveal('.username', {
+  origin: 'rigth',
+  distance: '500px', // A distancia do origin para a posição original do elemento.
+  duration: 1000, // A duração da animação em ms
+  delay: 600, // O delay para a animação acontecer
+  reset: false // Ao sair do campo de tela, a animação não acontecerá novamente caso eu passe por ela novamente.
+})
+
+sr.reveal('.whatsicon', {
+  origin: 'bottom',
+  distance: '400px', // A distancia do origin para a posição original do elemento.
+  duration: 1000, // A duração da animação em ms
+  delay: 200, // O delay para a animação acontecer
+  reset: false // Ao sair do campo de tela, a animação não acontecerá novamente caso eu passe por ela novamente.
+})
+sr.reveal('.cursoIcon', {
+  origin: 'bottom',
+  distance: '400px', // A distancia do origin para a posição original do elemento.
+  duration: 1000, // A duração da animação em ms
+  delay: 300, // O delay para a animação acontecer
+  reset: false // Ao sair do campo de tela, a animação não acontecerá novamente caso eu passe por ela novamente.
+})
+sr.reveal('.instaicon', {
+  origin: 'bottom',
+  distance: '400px', // A distancia do origin para a posição original do elemento.
+  duration: 1000, // A duração da animação em ms
+  delay: 400, // O delay para a animação acontecer
+  reset: false // Ao sair do campo de tela, a animação não acontecerá novamente caso eu passe por ela novamente.
+})
+sr.reveal('.faceicon', {
+  origin: 'bottom',
+  distance: '400px', // A distancia do origin para a posição original do elemento.
+  duration: 1000, // A duração da animação em ms
+  delay: 500, // O delay para a animação acontecer
+  reset: false // Ao sair do campo de tela, a animação não acontecerá novamente caso eu passe por ela novamente.
+})
+sr.reveal('.locIcon', {
+  origin: 'bottom',
+  distance: '400px', // A distancia do origin para a posição original do elemento.
+  duration: 1000, // A duração da animação em ms
+  delay: 600, // O delay para a animação acontecer
+  reset: false // Ao sair do campo de tela, a animação não acontecerá novamente caso eu passe por ela novamente.
+})
